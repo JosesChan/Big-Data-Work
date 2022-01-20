ce10f6375334bdac5c40c6cbd2317e72929498c8
Joses <joses.chan@gmail.com>
2022-01-20 15:55:24 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,98 +1,29 @@
 
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
-from sklearn.neighbors import NearestNeighbors
-from scipy.sparse import csr_matrix
-import findspark
 import pandas 
+import seaborn
 
-findspark.init()
+#pyspark matlablib and seaborn pandas
 
 spark = SparkSession.builder.getOrCreate()
 
 # Read csv file into spark
-ratings = spark.read.csv("dataset/ratings.csv",header=True)
-movies = spark.read.csv("dataset/movies.csv",header=True)
+nuclearPlantsSmall = spark.read.csv("dataset/nuclear_plants_small_dataset.csv",header=True)
 
-#Create dataframe with columns
-ratings = ratings.withColumn('userId', col('userId').cast('integer')). withColumn('movieId',col('movieId').cast('integer')). withColumn('rating', col('rating').cast('float')).drop('timestamp')
-ratings.show(5)
+# Task 1: Check to see if there are any missing values
 
-# Equijoin the movieId column in ratings and movies
-userRatings = ratings.join(movies, on='movieId')
-# Create dataframe with Pandas
-userRatings = userRatings.toPandas()
-# Create pivot table from dataframe
-userRatings = userRatings.pivot_table(index=['userId'], columns=['title'], values='rating')
-# Show first 5 rows
-userRatings.head()
+emptyDataRows = nuclearPlantsSmall[nuclearPlantsSmall.isnull().any(axis=1)]
 
-# Return tuple of the array dimensions in dataset of 610 users and 9719 movies
-userRatings.shape
+# Task 2: Normal Group and Abnormal Group, find min, max, mean, median, mode, variance and boxplot
 
-# Set neighbour amount for k-nearest neighbour amount
-kNeighbors = 10
+normalBoxplot = seaborn.boxplot(x='Sensors', y='Normal', data=nuclearPlantsSmall.loc[:, ['Power_range_sensor_1', 'Power_range_sensor_2', 'Power_range_sensor_3','Power_range_sensor_4','Pressure _sensor_1','Pressure _sensor_2','Pressure _sensor_3','Pressure _sensor_4','Vibration_sensor_1','Vibration_sensor_2','Vibration_sensor_3','Vibration_sensor_4']])
+seaborn.plt.show()
 
-# Pearson implementation
-# Calculate similarity using Pearson Correlation Coefficient
-userDistancePearson = userRatings.transpose().corr(method='pearson')
-
-# Keep rows and columns that have atleast k+1 neighbors and values that are non-NA.
-userDistancePearson = userDistancePearson.dropna(axis=0, thresh=kNeighbors+1).dropna(axis=1,
-thresh=kNeighbors+1)
-
-# Create square matrix, remove additional rows 
-userDistancePearson = userDistancePearson.loc[userDistancePearson.columns]
-
-# Calculate distance between users
-# Pearson Correlation Coefficient boundaries, 1 to -1, Pearson distance metric boundaries 0 to 2, Pearson distance = 1 - r
-userDistancePearson = 1.0 - userDistancePearson
-# account for floating point precision problem
-userDistancePearson[userDistancePearson < 0] = 0
-
-# k-nn forming neighborhoods 
-# metric set to precomputed, X is assumed to be a distance matrix and must be square during fit
-modelKNN = NearestNeighbors(metric='precomputed', algorithm='brute', n_neighbors=kNeighbors, n_jobs=-1)
-# fitting of X, X graph might be sparsely populated making non-zero elements the only canidates for being a neighbour
-modelKNN.fit(csr_matrix(userDistancePearson.fillna(0).values))
-# predict with k-nn model
-similarity, indexes = modelKNN.kNeighbors(csr_matrix(userDistancePearson.fillna(0).values),n_neighbors=kNeighbors)
-
-# average top 10 ratings using pearson
-# get neighbor id from user distance pearson dataframe as it currently stores the int index and not the actual id
-# then use K-nn imputation, calculates missing ratings
-# then calculate average top 10 from list
-neighborhoods_pearson = pandas.DataFrame({'neighborhood_ids':[userDistancePearson.iloc[neighbors].index.to_list() for neighbors in indexes],'distance': similarity.tolist()},index=userDistancePearson.index)
-user1NeighborsPearson = neighborhoods_pearson['neighborhood_ids'].loc[1]
-userRatings.loc[user1NeighborsPearson].mean().sort_values(ascending=False)[:10]
-
-# Spearman's rank correlation implementation
-# Calculate similarity using Pearson Correlation Coefficient
-userDistancesSpearman = userRatings.transpose().corr(method='spearman')
-
-# Keep rows and columns that have atleast k+1 neighbors and values that are non-NA.
-userDistancesSpearman = userDistancesSpearman.dropna(axis=0, thresh=kNeighbors+1).dropna(axis=1,thresh=kNeighbors+1)
-# Create square matrix, remove additional rows 
-userDistancesSpearman = userDistancesSpearman.loc[userDistancesSpearman.columns]
-
-# Calculate distance between users
-userDistancesSpearman = 1.0 - userDistancesSpearman
-userDistancesSpearman[userDistancesSpearman < 0] = 0
-
-# k-nn forming neighborhoods
-modelKNN = NearestNeighbors(metric='precomputed', algorithm='brute', n_neighbors=kNeighbors, n_jobs=-1)
-modelKNN.fit(csr_matrix(userDistancesSpearman.fillna(0).values))
-similarity, indexes = modelKNN.kNeighbors(csr_matrix(userDistancesSpearman.fillna(0).values),n_neighbors=kNeighbors)
-
-# average ratings using spearman's
-neighborhoods_spearman = pandas.DataFrame({'neighborhood_ids':[userDistancesSpearman.iloc[neighbors].index.to_list() for neighbors in indexes],'distance': similarity.tolist()}, index=userDistancesSpearman.index)
-user1NeighborsSpearman = neighborhoods_spearman['neighborhood_ids'].loc[1]
-userRatings.loc[user1NeighborsSpearman].mean().sort_values(ascending=False)[:10]
-
-#Both spearman and pearson recommendations examined through breakpoints
-#movies recommendation to user 1 based neighborhoods with pearson
-userRatings.loc[user1NeighborsPearson].mean().sort_values(ascending=False)[:10]
-#movies recommendation to user 1 based neighborhoods with spearman
-userRatings.loc[user1NeighborsSpearman].mean().sort_values(ascending=False)[:10]
-
+# Task 3: Show in a table the correlation matrix, where each element shows correlation between two features, find highly correlated features.
+# Task 4: Shuffle data into 70% training set and 30% test set
+# Task 5: Train a decision tree, svm and an artificial neural network. Evaluate classifiers by computing error rate (Incorrectly classified samples/Tota; Classified Samples), calculate sensitivity and specificity 
+# Task 6: Compare results based on task 5, which is best
+# Task 7: Discuss if features can detect abnormality in reactors
+# Task 8: Use mapReduce in pySpark to calculate minimum, maximum and mean for every feature
 
