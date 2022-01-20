b3694ae0920e3d32df88e1782a2aeb210e50b6fd
Joses <joses.chan@gmail.com>
2022-01-20 19:30:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,44 @@ pandasNuclearPlantsSmall = pandas.read_csv("dataset/nuclear_plants_small_dataset
 #emptyDataRows = nuclearPlantsSmall[nuclearPlantsSmall.isnull().any(axis=1)]
 
 # Task 2: Normal Group and Abnormal Group, find min, max, mean, median, mode, variance and boxplot
+columnNames = pandasNuclearPlantsSmall.drop(["Status"],axis = 1).columns.values
+
 pandasNormalNuclearPlantsSmall = pandasNuclearPlantsSmall.loc[pandasNuclearPlantsSmall["Status"] == "Normal"]
 pandasAbnormalNuclearPlantsSmall = pandasNuclearPlantsSmall.loc[pandasNuclearPlantsSmall["Status"] == "Abnormal"]
 
-seaborn.boxplot(x="Normal Sensors",data=pandasNormalNuclearPlantsSmall)
+
+for i in columnNames:
+    plt.figure()
+    seaborn.boxplot(x=pandasNuclearPlantsSmall["Status"], y=pandasNuclearPlantsSmall[i])
+    print("Normal" + i)
+    print("MINIMUM:") 
+    print(pandasNormalNuclearPlantsSmall[i].max())
+    print("MAX:") 
+    print(pandasNormalNuclearPlantsSmall[i].min())
+    print("MEAN:") 
+    print(pandasNormalNuclearPlantsSmall[i].mean())
+    print("MEDIAN:") 
+    print(pandasNormalNuclearPlantsSmall[i].median())
+    print("MODE:") 
+    print(pandasNormalNuclearPlantsSmall[i].mode())
+    print("VAR:") 
+    print(pandasNormalNuclearPlantsSmall[i].var())
+    print("Abnormal "+ i)
+    print("MINIMUM:") 
+    print(pandasAbnormalNuclearPlantsSmall[i].max())
+    print("MAX:") 
+    print(pandasAbnormalNuclearPlantsSmall[i].min())
+    print("MEAN:") 
+    print(pandasAbnormalNuclearPlantsSmall[i].mean())
+    print("MEDIAN:") 
+    print(pandasAbnormalNuclearPlantsSmall[i].median())
+    print("MODE:") 
+    print(pandasAbnormalNuclearPlantsSmall[i].mode())
+    print("VAR:") 
+    print(pandasAbnormalNuclearPlantsSmall[i].var())
+
+    plt.savefig('Graph '+i)
+
 plt.show()
 
 # Task 3: Show in a table the correlation matrix, where each element shows correlation between two features, find highly correlated features.
