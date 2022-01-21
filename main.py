3d66dbc9fcf906d62a5d0f714c361adfb677a53c
Joses <joses.chan@gmail.com>
2022-01-21 12:11:08 +0000

    Added correlation matrix

--- main.py
+++ main.py
@@ -37,42 +37,49 @@ pandasNormalNuclearPlantsSmall = pandasNuclearPlantsSmall.loc[pandasNuclearPlant
 pandasAbnormalNuclearPlantsSmall = pandasNuclearPlantsSmall.loc[pandasNuclearPlantsSmall["Status"] == "Abnormal"]
 
 
-for i in columnNames:
-    plt.figure()
-    seaborn.boxplot(x=pandasNuclearPlantsSmall["Status"], y=pandasNuclearPlantsSmall[i])
-    print("Normal" + i)
-    print("MINIMUM:") 
-    print(pandasNormalNuclearPlantsSmall[i].max())
-    print("MAX:") 
-    print(pandasNormalNuclearPlantsSmall[i].min())
-    print("MEAN:") 
-    print(pandasNormalNuclearPlantsSmall[i].mean())
-    print("MEDIAN:") 
-    print(pandasNormalNuclearPlantsSmall[i].median())
-    print("MODE:") 
-    print(pandasNormalNuclearPlantsSmall[i].mode())
-    print("VAR:") 
-    print(pandasNormalNuclearPlantsSmall[i].var())
-    print("Abnormal "+ i)
-    print("MINIMUM:") 
-    print(pandasAbnormalNuclearPlantsSmall[i].max())
-    print("MAX:") 
-    print(pandasAbnormalNuclearPlantsSmall[i].min())
-    print("MEAN:") 
-    print(pandasAbnormalNuclearPlantsSmall[i].mean())
-    print("MEDIAN:") 
-    print(pandasAbnormalNuclearPlantsSmall[i].median())
-    print("MODE:") 
-    print(pandasAbnormalNuclearPlantsSmall[i].mode())
-    print("VAR:") 
-    print(pandasAbnormalNuclearPlantsSmall[i].var())
-
-    plt.savefig('Graph '+i)
+# for i in columnNames:
+#     plt.figure()
+#     seaborn.boxplot(x=pandasNuclearPlantsSmall["Status"], y=pandasNuclearPlantsSmall[i])
+#     print("Normal" + i)
+#     print("MINIMUM:") 
+#     print(pandasNormalNuclearPlantsSmall[i].max())
+#     print("MAX:") 
+#     print(pandasNormalNuclearPlantsSmall[i].min())
+#     print("MEAN:") 
+#     print(pandasNormalNuclearPlantsSmall[i].mean())
+#     print("MEDIAN:") 
+#     print(pandasNormalNuclearPlantsSmall[i].median())
+#     print("MODE:") 
+#     print(pandasNormalNuclearPlantsSmall[i].mode())
+#     print("VAR:") 
+#     print(pandasNormalNuclearPlantsSmall[i].var())
+#     print("Abnormal "+ i)
+#     print("MINIMUM:") 
+#     print(pandasAbnormalNuclearPlantsSmall[i].max())
+#     print("MAX:") 
+#     print(pandasAbnormalNuclearPlantsSmall[i].min())
+#     print("MEAN:") 
+#     print(pandasAbnormalNuclearPlantsSmall[i].mean())
+#     print("MEDIAN:") 
+#     print(pandasAbnormalNuclearPlantsSmall[i].median())
+#     print("MODE:") 
+#     print(pandasAbnormalNuclearPlantsSmall[i].mode())
+#     print("VAR:") 
+#     print(pandasAbnormalNuclearPlantsSmall[i].var())
+#     print()
+#    plt.savefig('Graph '+i)
 
 plt.show()
 
 # Task 3: Show in a table the correlation matrix, where each element shows correlation between two features, find highly correlated features.
+
+print("Correlation Matrix")
+print(pandasNormalNuclearPlantsSmall.corr(method="pearson"))
+
 # Task 4: Shuffle data into 70% training set and 30% test set
+
+#trainingSet, testingSet = pandasNuclearPlantsSmall.randomsplit([0.7, 0.3])
+
 # Task 5: Train a decision tree, svm and an artificial neural network. Evaluate classifiers by computing error rate (Incorrectly classified samples/Tota; Classified Samples), calculate sensitivity and specificity 
 # Task 6: Compare results based on task 5, which is best
 # Task 7: Discuss if features can detect abnormality in reactors
