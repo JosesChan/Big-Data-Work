042a14c928147c754aa6b6e1dc944d63f7af8876
Joses <joses.chan@gmail.com>
2022-01-22 17:40:59 +0000

    Fixing dataframe used
    Fixing spark dataframe to infer and use headers given.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
@@ -8,23 +7,29 @@ from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.mllib.util import MLUtils
+from pyspark.mllib.classification import SVMWithSGD, SVMModel
+from pyspark.mllib.regression import LabeledPoint
 import pandas 
 import seaborn
 import matplotlib.pyplot as plt
-
+import pyspark.sql.functions
 spark = SparkSession.builder.getOrCreate()
 
 # Read csv file into spark
-sparksNuclearPlantsSmall = spark.read.csv("dataset/nuclear_plants_small_dataset.csv")
-
-#ratings = ratings.withColumn('userId', col('userId').cast('integer'))
-
 pandasNuclearPlantsSmall = pandas.read_csv("dataset/nuclear_plants_small_dataset.csv")
+sparksNuclearPlantsSmall=spark.read.csv("dataset/nuclear_plants_small_dataset.csv", header=True,inferSchema=True)
+
 
 
 # Task 1: Check to see if there are any missing values
+# null values in each column
 
-#emptyDataRows = nuclearPlantsSmall[nuclearPlantsSmall.isnull().any(axis=1)]
+dataframeNaN = pandasNuclearPlantsSmall[pandasNuclearPlantsSmall.isna().any(axis=1)]
+if(dataframeNaN.empty):
+    print("No null values")
+else:
+    print("Null values\n")
+    print(dataframeNaN)
 
 # Task 2: Normal Group and Abnormal Group, find min, max, mean, median, mode, variance and boxplot
 columnNames = pandasNuclearPlantsSmall.drop(["Status"],axis = 1).columns.values
@@ -76,9 +81,9 @@ pandasAbnormalNuclearPlantsSmall = pandasNuclearPlantsSmall.loc[pandasNuclearPla
 
 # Index labels, adding metadata (for identification) to the label column
 # Fit the entire dataset, including all labels in index
-labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(sparksNuclearPlantsSmall)
+labelIndexer = StringIndexer(inputCol="Status", outputCol="StatusIndexedLabel").fit(sparksNuclearPlantsSmall)
 
-# Getting our y value, feature list
+# Inputting x value
 # Automatically identify categorical features, and index them seperately.
 # Using maxCategories, we can select features with > 4 distinct values to be treated as continuous 
 # (Continous instead of discrete, infinite number of values between two values) (Status label has 2 distinct known values currently, can be considered discrete).
@@ -87,66 +92,74 @@ featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures",
 # Split the data into training and test sets (30% held out for testing)
 (trainingData, testData) = sparksNuclearPlantsSmall.randomSplit([0.7, 0.3])
 
-
 # Task 5: Train a decision tree, svm and an artificial neural network. Evaluate classifiers by computing error rate (Incorrectly classified samples/Total Classified Samples), calculate sensitivity and specificity 
 
 # Change into this
 #https://spark.apache.org/docs/1.5.2/ml-decision-tree.html
 
-# Instance of Decision tree classifier
-treeClf = DecisionTreeClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
+# # Instance of Decision tree classifier
+# treeClf = DecisionTreeClassifier(labelCol="StatusIndexedLabel", featuresCol="indexedFeatures")
 
-# Link the indexers and tree into a pipeline
-# Pipeline to create stages
-# Ensuring data goes through identical processing steps
-pipeline = Pipeline(stages=[labelIndexer, featureIndexer, treeClf])
+# # Link the indexers and tree into a pipeline
+# # Pipeline to create stages
+# # Ensuring data goes through identical processing steps
+# pipeline = Pipeline(stages=[labelIndexer, featureIndexer, treeClf])
 
-# Train model, pipeline estimator stage which produces a model which is a transformer. Running the indexer through the stages
-model = pipeline.fit(trainingData)
+# # Train model, pipeline estimator stage which produces a model which is a transformer. Running the indexer through the stages
+# treeModel = pipeline.fit(trainingData)
 
-# Predict results, pipeline transformer stage where the model makes the predictions from the dataset
-predictions = model.transform(testData)
+# # Predict results, pipeline transformer stage where the model makes the predictions from the dataset
+# predictions = treeModel.transform(testData)
 
-# Select example rows to display.
-predictions.select("prediction", "indexedLabel", "features").show(5)
+# # Select example rows to display.
+# predictions.select("prediction", "indexedLabel", "features").show(5)
 
-# Compute error rate
-evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel", predictionCol="prediction", metricName="precision")
-accuracy = evaluator.evaluate(predictions)
-print ("Decision Tree Test Error = %g" % (1.0 - accuracy))
+# # Compute error rate
+# evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel", predictionCol="prediction", metricName="precision")
+# accuracy = evaluator.evaluate(predictions)
+# print ("Decision Tree Test Error = %g" % (1.0 - accuracy))
 
-treeModel = model.stages[2]
-print (treeModel) 
-
-
+# treeModelShow = treeModel.stages[2]
+# print (treeModelShow) 
 
 
 
+# # Build the model
+# svmModel = SVMWithSGD.train(trainingData, iterations=100)
+
+# # Evaluating the model on training data
+# labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
+# trainErr = labelsAndPreds.filter(lambda lp: lp[0] != lp[1]).count() / float(parsedData.count())
+# print("Training Error = " + str(trainErr))
+
+# # Save and load model
+# svmModel.save(sc, "target/tmp/pythonSVMWithSGDModel")
+# sameModel = SVMModel.load(sc, "target/tmp/pythonSVMWithSGDModel")
 
 
 
 
 
-# Instance of Support Vector Machines classifier
-svmClf = svm.SVC()
-svmClf.fit(xTrain, yTrain)
-yPredictSvmClf = svmClf.predict(xTest)
-accuracy = zero_one_loss(yTest, yPredictSvmClf)
-svmErrorRate = 1 - accuracy
+# # Instance of Support Vector Machines classifier
+# svmClf = svm.SVC()
+# svmClf.fit(xTrain, yTrain)
+# yPredictSvmClf = svmClf.predict(xTest)
+# accuracy = zero_one_loss(yTest, yPredictSvmClf)
+# svmErrorRate = 1 - accuracy
 
-# Instance of multilayer perceptron, type of artificial neural network
-annClf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-annClf.fit(xTrain, yTrain)
-yPredictAnnClf = annClf.predict(xTest)
-accuracy = zero_one_loss(yTest, yPredictAnnClf)
-annErrorRate = 1 - accuracy
+# # Instance of multilayer perceptron, type of artificial neural network
+# annClf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+# annClf.fit(xTrain, yTrain)
+# yPredictAnnClf = annClf.predict(xTest)
+# accuracy = zero_one_loss(yTest, yPredictAnnClf)
+# annErrorRate = 1 - accuracy
 
 
-# Task 6: Compare results based on task 5, which is best
+# # Task 6: Compare results based on task 5, which is best
 
-print("Error rate for decision tree" + treeErrorRate)
-print("Error rate for support vector machines" + svmErrorRate)
-print("Error rate for artificial neural network" + annErrorRate)
+# print("Error rate for decision tree" + treeErrorRate)
+# print("Error rate for support vector machines" + svmErrorRate)
+# print("Error rate for artificial neural network" + annErrorRate)
 
 # Task 7: Discuss if features can detect abnormality in reactors
 # Task 8: Use mapReduce in pySpark to calculate minimum, maximum and mean for every feature
